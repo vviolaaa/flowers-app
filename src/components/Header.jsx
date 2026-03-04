2996445e57124ad0e354e20ef4f28a2872118280
Viola Stan <violastan22@gmail.com>
2026-03-05 00:26:52 +0200

    Add Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import "../index.css"
+
+
+export function Header() {
+    return (
+        <div className="header-container">
+            <h1>Imagine that smth cool is written here</h1>
+        </div>
+
+    );
+}
