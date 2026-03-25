45d6908113464d246eab16690dd0e50ff2cd8024
Viola Stan <violastan22@gmail.com>
2026-03-25 22:03:16 +0200

    fix Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,10 +3,17 @@ import "../index.css"
 export function Header({ username, onLogout, onAuthOpen }) {
     return (
         <div className="header-container">
-<<<<<<<<< Temporary merge branch 1
             <h1>Imagine that smth cool is written here - MASTER VER.</h1>
-=========
-            
+            <div className="header-auth">
+                {username ? (
+                    <>
+                        <span className="header-username">{username}</span>
+                        <button className="header-btn logout" onClick={onLogout}>Log out</button>
+                    </>
+                ) : (
+                    <button className="header-btn login" onClick={onAuthOpen}>Login / Sign up</button>
+                )}
+            </div>
         </div>
     );
 }
