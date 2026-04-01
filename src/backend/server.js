a4b6aeda0c4cc71f8d8a6db67fb09f8ed493c94e
Viola Stan <violastan22@gmail.com>
2026-04-01 14:07:56 +0300

    add SERVER_SECRET to workflow

--- src/backend/server.js
+++ src/backend/server.js
@@ -7,7 +7,7 @@ const path = require('path');
 
 const app = express();
 const PORT = 5000;
-const SECRET = 'your_secret_key_change_this';
+process.env.SERVER_SECRET || 'wildflower_secret_key';
 const DB_PATH = path.join(__dirname, 'db.json');
 
 app.use(cors());
